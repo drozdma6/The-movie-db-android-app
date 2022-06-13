d7913f1b340f1704d81894f0898779d7a09d1599
drozdma6 <drozdma6@cvut.cz>
2022-06-14 01:04:14 +0200

    Adding netflix logo

--- app/src/main/java/cz/cvut/fit/drozdma6/semestral/MainActivity.kt
+++ app/src/main/java/cz/cvut/fit/drozdma6/semestral/MainActivity.kt
@@ -1,11 +1,29 @@
 package cz.cvut.fit.drozdma6.semestral
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import cz.cvut.fit.drozdma6.semestral.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val destinationIdsWithoutToolbar = setOf(
+        R.id.movieDetailFragment
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val isDestinationWithoutToolbar = destination.id in destinationIdsWithoutToolbar
+            binding.netflixImg.isVisible = !isDestinationWithoutToolbar
+        }
     }
 }
